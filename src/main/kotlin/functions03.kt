13423b801010d0c695a3ef4872df2337515e93f6
Ulyanenko Savely <ulyanenko@surf.dev>
2023-11-13 22:22:43 +0300

    Закончил задания по блоку 'Функции и рекурсия'

--- src/main/kotlin/functions03.kt
+++ src/main/kotlin/functions03.kt
@@ -34,5 +34,66 @@ fun main() {
     sayHello()
     sayHello("Саша")
 
+    // функция isGreater для сравнения чисел, пар чисел
+    infix fun Int.isGreater(other: Int): Boolean {
+        return this > other
+    }
+
+    val greaterNumber = 100 isGreater 5
+    println("Число 100 больше 5?: $greaterNumber")
+    val pairOfNumbers1 = Pair(11, 500)
+    val pairOfNumbers2 = Pair(10000, -5)
+    val greaterNumberInPairOne = pairOfNumbers1.first isGreater pairOfNumbers1.second
+    val greaterNumberInPairTwo = pairOfNumbers2.first isGreater pairOfNumbers2.second
+    println("Сравнение первой пары числе, 11 больше 500?: $greaterNumberInPairOne")
+    println("Сравнение первой пары числе, 10000 больше -5?: $greaterNumberInPairTwo")
+
+    // рекурсивная функция generateSequence
+    tailrec fun generateSequence(n: Int, counter: Int = 0): Int {
+        // остановка функции при достижении 0, иначе функция будет бесконечно вызывать саму себя
+        if (n <= 0) {
+            return counter
+        }
+        println(counter)
+        return generateSequence(n - 1, counter + 1)
+    }
+
+    val sequenceResult = generateSequence(15)
+
+    // функции let, apply, with, also, run
+    val listOfNames = mutableListOf<String>("Кошка", "Жираф", "Бегемот", "Собака")
+
+    listOfNames.let {
+        it.add("Гиппопотам")
+        val addedName = listOfNames[4]
+        println("Добавили новое животное: $addedName")
+    }
+
+    // перевернем массив
+    listOfNames.run {
+        this.reverse()
+        println(this)
+    }
+
+    // удалим один элемент из массива
+    listOfNames.apply {
+        remove(this[1])
+        println(this)
+    }
+
+    // повторим 3 элемент массива несколько раз
+    listOfNames.also {
+        it[2] = it[2].repeat(3)
+        println(it)
+    }
+
+    // проверка массива на пустоту
+    with(listOfNames) {
+        if (isEmpty()) {
+            println("Массив пустой")
+        } else {
+            println("Массив не пустой и содержит имена: $this")
+        }
+    }
 
 }
