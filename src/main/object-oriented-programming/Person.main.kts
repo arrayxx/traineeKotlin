e8a0a37a28fc7fdb09629c2b9af667a419c0ec0b
Ulyanenko Savely <ulyanenko@surf.dev>
2023-11-24 00:36:14 +0300

    4. Создайте класс Person с приватными свойствами name, age и публичными методами getName() и getAge(). Проверьте, что прямой доступ к этим свойствам невозможен извне класса.

---
+++ src/main/object-oriented-programming/Person.main.kts
@@ -0,0 +1,29 @@
+class Person {
+    // создадим дополнительный объект внутри класса
+    companion object {
+        // сделаем переменные приватными, т.е. видимыми только внутри класса
+        // переменные должны быть доступны при вызове методов класса извне класса
+        // т.к. companion object - это публичный объект
+        private val name = "Савелий"
+        private val age = 24
+    }
+    fun getName(): String {
+        return name
+    }
+    fun getAge(): Int {
+        return age
+    }
+}
+
+// проверим, что свойства можно вызвать через метод
+fun accessToQualities() {
+    println(Person().getName())
+    println(Person().getAge())
+}
+
+accessToQualities()
+
+// проверяем, что вызов параметров name и age недоступен напрямую
+// получим ошибку error: cannot access 'name':/'age': it is private in 'Companion'
+Person.name
+Person.age
